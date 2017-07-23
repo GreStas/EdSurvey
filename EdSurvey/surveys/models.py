990252092c22cb5695000da73ce82a358b74e1e9
GreStas <grestas2000@gmail.com>
2017-07-23 13:12:34 +0300

    Переделана модель surveys.Result под multi-table inheritance

--- EdSurvey/surveys/models.py
+++ EdSurvey/surveys/models.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from questions.models import Question, Answer
+from questions.models import Question, AnswerRB, AnswerCB, AnswerLL
 from schedules.models import Task, Schedule
 
 
@@ -27,8 +28,8 @@ class Result(models.Model):
     class Meta:
         verbose_name = 'Результат'
         verbose_name_plural = 'Результаты'
-        #unique_together = ('user', 'attempt', 'question')
-        unique_together = ('attempt', 'question', 'answer')
+        # unique_together = ('user', 'attempt', 'question')
+        # unique_together = ('attempt', 'question')
 
     def __str__(self):
         return "#{}.{}".format(self.attempt, str(self.question))
@@ -36,7 +37,64 @@ class Result(models.Model):
     # user = models.ForeignKey('auth.user') # Пользоватеь
     attempt = models.ForeignKey(Attempt)    #   в ходе попытки
     question = models.ForeignKey(Question)  #     на вопрос
-    answer = models.ForeignKey(Answer)      #       дал ответ
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     # status = models.SmallIntegerField()
+
+
+class ResultRB(Result):
+    """ qtype == CB
+        необходим для создания отдельных валидаторов
+    """
+    class Meta:
+        verbose_name = 'Одиночный Ответ (RadioButton)'
+        verbose_name_plural = 'Одиночные Ответы (RadioButton)'
+
+    result_ptr = models.OneToOneField(
+        Result, on_delete=models.CASCADE,
+        parent_link=True,
+    )
+    answerRB = models.ForeignKey(AnswerRB)  #       дал ответ
+
+    def clean(self):
+        qtype = self.question.qtype
+        if qtype != 'RB':
+            raise ValidationError("Тип вопроса ({}) и тип ответа (RB) не совпадают.".format(qtype))
+
+
+class ResultCB(Result):
+    """ qtype == CB
+        необходим для создания отдельных валидаторов
+    """
+    class Meta:
+        verbose_name = 'Множественный Ответ (CheckBox)'
+        verbose_name_plural = 'Множественные Ответы (CheckBox)'
+
+    result_ptr = models.OneToOneField(
+        Result, on_delete=models.CASCADE,
+        parent_link=True,
+    )
+    answerCB = models.ForeignKey(AnswerCB)  #       дал ответ
+
+    def clean(self):
+        qtype = self.question.qtype
+        if qtype != 'CB':
+            raise ValidationError("Тип вопроса ({}) и тип ответа (CB) не совпадают.".format(qtype))
+
+
+class ResultLL(Result):
+    class Meta:
+        verbose_name = 'Ответ-Путанка'
+        verbose_name_plural = 'Ответы-Путанки'
+
+    result_ptr = models.OneToOneField(
+        Result, on_delete=models.CASCADE,
+        parent_link=True,
+    )
+    answerLL = models.ForeignKey(AnswerLL)  #       дал ответ
+    itemLL = models.ForeignKey(AnswerLL, related_name='+')    #         выбрав вариант
+
+    def clean(self):
+        qtype = self.question.qtype
+        if qtype != 'LL':
+            raise ValidationError("Тип вопроса ({}) и тип ответа (LL) не совпадают.".format(qtype))
